365969320c88af8017a56cc35f198029b5c080d2
justinkuhn <justintkuhn@gmail.com>
2019-07-20 09:11:56 -0400

    recent update from glitch

--- js/cabin-events.js
+++ js/cabin-events.js
@@ -1,1 +1,67 @@
 
+ AFRAME.registerComponent('tractor-beam-book', {
+      schema: {},
+
+      init: function() {
+        var el = this.el;
+        el.addEventListener("raycaster-intersected", handler, true);
+        el.addEventListener('raycaster-intersected-cleared', function h(e) {
+          el.removeEventListener('raycaster-intersected', handler, true)
+          el.removeEventListener('raycaster-intersected-cleared', h)
+        }.bind(this))
+        
+        function handler() {
+        
+          position = document.getElementById('camera').getAttribute('position');
+            
+          
+            
+            el.setAttribute('animation__pos', {
+                property: 'position',
+                to: {x:position.x,y:position.y,z:position.z-1},
+                dur: 5000,
+                easing: 'easeInOutCubic'
+            });
+          
+          el.emit('open');
+
+          }
+      }
+    });
+
+AFRAME.registerComponent('death-interaction', {
+  init: function() {
+    var el = this.el;
+    
+    el.addEventListener("raycaster-intersected", deathhandler, true);
+    el.addEventListener("raycaster-intersection-cleared", function() {
+//      clear timeout
+    })
+    
+    function deathhandler() {
+//       turn on audio
+      var deathSound = document.getElementById('death-sound');
+      var playerSound = document.getElementById('azure-sound');
+      console.log(deathSound);
+      console.log("turning on audio...");
+      deathSound.play();
+      playerSound.play();
+      // wait until audio is finished
+      deathSound.addEventListener("stopped", function() {
+        
+        //      fade out and return to start screen
+        window.location.href ='index.html';
+      });
+      
+    }
+  }
+  
+});
+
+
+// Things to do:
+
+// 1. allow door to be opened once book is opened and closed
+// 2. closing animation for book
+// 3. lighting control
+// 4. populate book with recipe items
